df181259a1eccef3a724a7fb017a27fe10a36f7a
M4TThys123 <70703948+M4TThys123@users.noreply.github.com>
2022-10-25 14:14:30 +0200

    🎉 Initialized React App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mijn naam is Matthijs</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
